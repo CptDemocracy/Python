d6b0f8f8bfd57bad0fa05dd0f0a99e2de830e0bc
CptDemocracy <profit234@gmail.com>
2016-03-31 02:02:33 +0300

    solution to problem 7c

---
+++ MITx-6.00.1x-EDX-Introduction-to-Computer-Science/Week-3/Lecture-6/problem7c.py
@@ -0,0 +1,29 @@
+"""
+Problem 7b.
+
+Here is the code for a function applyToEach:
+
+def applyToEach(L, f):
+    for i in range(len(L)):
+        L[i] = f(L[i])
+
+Assume that:
+  testList = [1, -4, 8, -9]
+
+For each of the questions [editor's comment: q. 7a, 7b, 7c] 
+(which you may assume is evaluated independently of the 
+previous questions, so that testList has the value indicated 
+above), provide an expression using applyToEach, so that after 
+evaluation testList has the indicated value. You may need to 
+write a simple procedure in each question to help with this 
+process.
+
+Q.:
+>>> print testList
+[1, 16, 64, 81]
+"""
+
+def square(n):
+    return pow(n, 2)
+
+applyToEach(testList, square)
