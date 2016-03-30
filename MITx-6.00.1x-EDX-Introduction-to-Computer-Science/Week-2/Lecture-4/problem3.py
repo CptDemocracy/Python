702c53dec8b6246aa79cc08a2a73ca55926fe14e
CptDemocracy <profit234@gmail.com>
2016-03-31 01:05:42 +0300

    problem 3 solution

---
+++ MITx-6.00.1x-EDX-Introduction-to-Computer-Science/Week-2/Lecture-4/problem3.py
@@ -0,0 +1,14 @@
+"""
+Problem 3.
+
+Write a Python function, square, that takes in one number 
+and returns the square of that number.
+
+This function takes in one number and returns one number.
+"""
+
+def square(x):
+    '''
+    x: int or float.
+    '''
+    return x ** 2
