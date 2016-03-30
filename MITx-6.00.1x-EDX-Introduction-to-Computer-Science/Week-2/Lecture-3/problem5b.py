8aa82057bbbd6dfb56c948c20967a97a5a4c833b
CptDemocracy <profit234@gmail.com>
2016-03-31 00:53:00 +0300

    problem 5b solution

---
+++ MITx-6.00.1x-EDX-Introduction-to-Computer-Science/Week-2/Lecture-3/problem5b.py
@@ -0,0 +1,16 @@
+"""
+Problem 5b.
+
+Convert the following code into code that uses a for loop:
+  print "Hello!"
+  print 10
+  print 8
+  print 6
+  print 4
+  print 2
+  
+"""
+
+print 'Hello!'
+for n in range(10, 1, -2):
+    print n
