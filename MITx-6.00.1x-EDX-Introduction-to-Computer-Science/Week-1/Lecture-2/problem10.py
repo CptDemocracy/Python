7cf77b4f1504abd74ee4ff2ea6214afe6e39ebc3
CptDemocracy <profit234@gmail.com>
2016-03-31 00:41:07 +0300

    problem 10 solution

--- MITx-6.00.1x-EDX-Introduction-to-Computer-Science/Week-1/Lecture-2/problem10.py
+++ MITx-6.00.1x-EDX-Introduction-to-Computer-Science/Week-1/Lecture-2/problem10.py
@@ -1,6 +1,6 @@
 """
 Write a piece of Python code that prints out the string 'hello world' 
-if the value of an integer variable, happy, is strictly greater than 2.
+if the value of an integer variable, happy, is strictly greater than 2. 
 """
 
 s = "hello world"
