c9bf2ce7b7db8623a8182ea138531b7e79820874
CptDemocracy <profit234@gmail.com>
2016-03-31 00:52:08 +0300

    problem 5a solution

---
+++ MITx-6.00.1x-EDX-Introduction-to-Computer-Science/Week-2/Lecture-3/problem5a.py
@@ -0,0 +1,19 @@
+"""
+Problem 5a.
+
+In this problem you'll be given a chance to practice writing 
+some for loops.
+
+Convert the following code into code that uses a for loop:
+
+  print 2
+  print 4
+  print 6
+  print 8
+  print 10
+  print "Goodbye!"
+"""
+
+for n in range(2, 11, 2):
+    print str(n)
+print 'Goodbye!'
