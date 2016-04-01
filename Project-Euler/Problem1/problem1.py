b969a29f7fb4d0123f8677bd6857a66c926e556e
CptDemocracy <profit234@gmail.com>
2016-04-01 13:12:03 +0300

    Update problem1.py

--- Project-Euler/Problem1/problem1.py
+++ Project-Euler/Problem1/problem1.py
@@ -1,12 +1,9 @@
 """
 [ref.href] https://projecteuler.net/problem=1
-
 Multiples of 3 and 5.
-
 If we list all the natural numbers below 10 that are multiples
 of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is
 23.
-
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
@@ -22,5 +19,6 @@ def func(nums, upperBound):
 upperBound = 1000
 muls = [3, 5]
 sm = func([3, 5], 1000)
-print "The sum of multiples of " + " and ".join([str(mul) for mul in muls])\
-        + " below " + str(upperBound) + " is equal to " + str(sm) + "."
+print "The sum of multiples of %s below %d is equal to %d." % (" and ".join([str(mul) for mul in muls]), 
+                                                               upperBound, 
+                                                               sm)
