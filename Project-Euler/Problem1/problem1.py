41b00dd554c918eedc6034dd1854db2e34ffe322
CptDemocracy <profit234@gmail.com>
2016-04-01 07:21:13 +0300

    Create problem1.py

---
+++ Project-Euler/Problem1/problem1.py
@@ -0,0 +1,26 @@
+"""
+[ref.href] https://projecteuler.net/problem=1
+
+Multiples of 3 and 5.
+
+If we list all the natural numbers below 10 that are multiples
+of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is
+23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+def func(nums, upperBound):
+    mulSet = set()
+    for num in nums:
+        product = num
+        while product < upperBound:
+            mulSet.add(product)
+            product += num
+    return sum(mulSet)
+
+upperBound = 1000
+muls = [3, 5]
+sm = func([3, 5], 1000)
+print "The sum of multiples of " + " and ".join([str(mul) for mul in muls])\
+        + " below " + str(upperBound) + " is equal to " + str(sm) + "."
